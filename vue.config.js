6cfd85597b5ce492461f9111f266771932e1659b
YolandaKisses <jelare_svken@qq.com>
2022-09-19 16:40:12 +0800

    fix 工具类打包

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,19 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path')
 module.exports = {
   publicPath: "./",
   assetsDir: 'static',
   productionSourceMap: false, // 是否在构建生产包时生成sourcdeMap
-   lintOnSave: false, // 是否开启eslint保存检测
+  lintOnSave: false, // 是否开启eslint保存检测
+  configureWebpack: {
+    plugins: [
+      new CopyWebpackPlugin([
+        {
+          from: path.resolve(__dirname, 'src/utils'),
+          to: path.resolve(__dirname, 'dd-ui-library/utils'),
+          ignore: ".*"
+        }
+      ])
+    ]
+  }
 }

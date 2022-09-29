468dd2a1056635b2e79f8890ec44b36c25f926c2
sun-yun-long <1621563513@qq.com>
2022-09-29 11:33:12 +0800

    myPromiseAll

--- src/utils/index.js
+++ src/utils/index.js
@@ -125,6 +125,49 @@ export function convertToOptions(
   return _result;
 }
 
+function PromiseObj(item){
+  return new Promise((resolve, reject)=>{
+    let obj = {};
+    if(item.method == "get"){
+      obj = {
+        url: item.url + '?' + qs.stringify(item.params),
+        method: 'get'
+      }
+    }
+    if(item.method == "post"){
+      if(item.paramsType == "json"){
+        obj = {
+          url: item.url,
+          method: 'post',
+          data: item.params
+        }
+      }
+      if(item.paramsType == "formdata"){
+        obj = {
+          url: item.url,
+          method: 'post',
+          data: qs.stringify(item.params)
+        }
+      }
+    }
+    request(obj).then(res=>{
+      resolve({ key:item.key, data:res.data })
+    })
+  })
+}
+
+/**
+ * Promise.all 封装
+ * @param {Array} array 
+ */
+export function myPromiseAll(array){
+  let AllList = []
+  array.forEach(item=>{
+    AllList.push(PromiseObj(item))
+  })
+  return Promise.all(AllList)
+}
+
 export default {
   listToTree,
   convertToDictLabel,
@@ -133,5 +176,6 @@ export default {
   setToken,
   removeToken,
   getSqlDataBySqlCode,
-  dictionaryList
+  dictionaryList,
+  myPromiseAll
 }
